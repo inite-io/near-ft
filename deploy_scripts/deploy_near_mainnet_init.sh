4de8ad12dae7f98a8e0b207b88f75522504f7f80
Andrei Ivanouski <aivanouski@gmail.com>
2022-09-19 21:46:23 +0300

    Update deploy_near_mainnet_init.sh

--- deploy_scripts/deploy_near_mainnet_init.sh
+++ deploy_scripts/deploy_near_mainnet_init.sh
@@ -2,6 +2,6 @@ rm -rf neardev/
 cp ./ft/src/lib_init.rs ./ft/src/lib.rs
 ./build.sh
 near deploy --wasmFile res/fungible_token.wasm --helperUrl https://near-contract-helper.onrender.com --accountId $CONTRACT_NAME
-near call $CONTRACT_NAME new '{"owner_id": "'$CONTRACT_NAME'", "total_supply": "0", "metadata": { "spec": "ft-1.0.0", "name": "AppTestnet INIT Token", "symbol": "INIT", "decimals": 24 }}' --accountId $CONTRACT_NAME
+near call $CONTRACT_NAME new '{"owner_id": "'$CONTRACT_NAME'", "total_supply": "1000000000", "metadata": { "spec": "ft-1.0.0", "name": "INIT Token", "symbol": "INIT", "decimals": 24 }}' --accountId $CONTRACT_NAME
 
-echo "Deployed to local near (ERC20 - INIT TOKEN) = $CONTRACT_NAME"
+echo "Deployed to near (INIT TOKEN) = $CONTRACT_NAME"
