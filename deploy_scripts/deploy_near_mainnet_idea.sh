775490cf2e095c1c963ff27598d43965f05feec8
Andrei Ivanouski <aivanouski@gmail.com>
2022-09-19 21:43:30 +0300

    Update deploy_near_mainnet_idea.sh

--- deploy_scripts/deploy_near_mainnet_idea.sh
+++ deploy_scripts/deploy_near_mainnet_idea.sh
@@ -2,6 +2,6 @@ rm -rf neardev/
 cp ./ft/src/lib_idea.rs ./ft/src/lib.rs
 ./build.sh
 near deploy --wasmFile res/fungible_token.wasm --helperUrl https://near-contract-helper.onrender.com --accountId $CONTRACT_NAME
-near call $CONTRACT_NAME new '{"owner_id": "'$CONTRACT_NAME'", "total_supply": "0", "metadata": { "spec": "ft-1.0.0", "name": "AppTestnet IDEA Token", "symbol": "IDEA", "decimals": 24 }}' --accountId $CONTRACT_NAME
+near call $CONTRACT_NAME new '{"owner_id": "'$CONTRACT_NAME'", "total_supply": "0", "metadata": { "spec": "ft-1.0.0", "name": "IDEA Token", "symbol": "IDEA", "decimals": 24 }}' --accountId $CONTRACT_NAME
 
-echo "Deployed to local near (ERC20 - IDEA TOKEN) = $CONTRACT_NAME"
+echo "Deployed to near (IDEA TOKEN) = $CONTRACT_NAME"
